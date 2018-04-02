c4171d6d02905476f8d32413c37a0a58bef943db
eugenexlin <eugenexlin@gmail.com>
2018-04-01 18:16:47 -0700

    iinitial dump before GCE stuff

---
+++ joke-receptacle/src/main/java/com/djdenpa/joke_receptacle/JokeReceiverActivity.java
@@ -0,0 +1,37 @@
+package com.djdenpa.joke_receptacle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by denpa on 3/24/2018.
+ */
+
+public class JokeReceiverActivity extends AppCompatActivity {
+  public static final String EXTRA_JOKE = "com.djdenpa.joke_receptacle_EXTRA_JOKE";
+
+
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_joke_receptacle);
+
+    TextView tv_joke = findViewById(R.id.tv_joke);
+
+    Intent intent = getIntent();
+    if(intent.hasExtra(EXTRA_JOKE)) {
+      String joke = intent.getExtras().getString(EXTRA_JOKE);
+      tv_joke.setText(joke);
+    }else{
+      String message = getString(R.string.no_joke_to_show);
+      tv_joke.setText(message);
+    }
+
+  }
+
+
+}
---
+++ joke-supply/src/main/java/com/djdenpa/joke_supply/JokingJoker.java
@@ -0,0 +1,7 @@
+package com.djdenpa.joke_supply;
+
+public class JokingJoker {
+  public String tellJoke() {
+    return "ayy lmao";
+  }
+}
